001ab5c79a5023efac970abe360fd63fee75b8ba
Rafael C <rafael.castro@upc.edu.pe>
2025-09-09 12:10:57 -0500

    feat(shared): add share bounded context

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import {Currency} from "./shared/domain/model/currency";
+
 console.log('Happy developing ✨')
+
+try{
+    // Scenario 1: Real-time registration with USD and current date
+    const usdCurrencyCode = "USD" as const;
+    const usdCurrency = new Currency(usdCurrencyCode);
+    console.log(usdCurrency.toString());
+
+} catch(e){
+    console.log(e);
+}
